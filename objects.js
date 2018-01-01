66389843f5385deba9d6689f44dba29e5ace295d
Kelsey M. Beard <kay.beard.design@gmail.com>
2018-01-01 07:48:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,11 @@
-var playlist = new Object({});
-playlist = {
-  artistName: "Madonna",
-  songTitle: "Like A Virgin"
-};
+var playlist = { artistName: "songTitle" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return playlist['Phil Ochs'] = "Here's to the State of Mississippi";
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi";
   return playlist;
 };
 
 function removeFromPlaylist(playlist, artistName) {
+  delete playlist.Kanye
   return
 };
