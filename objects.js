d87cfaef0db17c3830909535414746ca2b69c142
Kelsey M. Beard <kay.beard.design@gmail.com>
2018-01-01 06:46:55 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = new Object();
+playlist = {
+  Maddona: "Like A Virgin"
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return playlist["artist"] = artistName;
+};
+
+function removeFromPlaylist(playlist, artistName) {
+  return
+};
